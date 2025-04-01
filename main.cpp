f6d919fa414f6b510dd56a6fecd7e9dbb751ad42
HarolDeveloper <hlancherosalvarez@gmail.com>
2025-03-31 20:34:08 -0600

    issues fixed

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
-/**
- * Programa que genera y ordena sufijos de un texto
- * Ordena alfabéticamente todos los sufijos e imprime sus posiciones originales
- * Autor: Harold Andres Lancheros Alvarez - A00836416
- * Fecha: 09/03/2025
+
+/*
+ * Programa que genera y ordena sufijos de un texto.
+ * Ordena alfabéticamente todos los sufijos e imprime sus posiciones originales.
+ * Copyright (C) 2024 Harold Andres Lancheros Alvarez
+ * A00836416
+ *
+ * Este programa es software libre; puede redistribuirlo y/o
+ * modificarlo bajo los términos de la Licencia Pública General Reducida GNU
+ * tal como está publicada por la Free Software Foundation; ya sea
+ * la versión 3 de la Licencia, o (a su elección) cualquier versión posterior.
+ *
+ * Este programa se distribuye con la esperanza de que sea útil,
+ * pero SIN NINGUNA GARANTÍA; sin siquiera la garantía implícita de
+ * COMERCIABILIDAD o IDONEIDAD PARA UN PROPÓSITO PARTICULAR. Consulte la GNU
+ * Lesser General Public License para más detalles.
+ *
+ * Debería haber recibido una copia de la Licencia Pública General Reducida GNU
+ * junto con este programa; si no es así, escriba a la Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 #include <iostream>
 #include <vector>
@@ -20,7 +35,7 @@ int main()
     getline(cin, textoEntrada);
 
     // Crear vector de pares (sufijo, posición original)
-    vector<pair<string, int> > vectorSufijos;
+    vector<pair<string, int>> vectorSufijos;
     for (int i = 0; i < textoEntrada.length(); i++)
     {
         vectorSufijos.push_back(make_pair(textoEntrada.substr(i), i + 1));
